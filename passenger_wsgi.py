d5cf92adde25fecc7188c2dd3151337fa873bd99
Evgen Polivanov <polivanov004@gmail.com>
2026-02-13 19:59:39 +0300

    xxx

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/home/p/polivajh/polivajh.beget.tech/public_html')
-sys.path.insert(1, '/home/p/polivajh/polivajh.beget.tech/public_html/venv/lib/python3.10/site-packages')
+sys.path.insert(0, '/home/p/polivajh/prosvet/public_html')
+sys.path.insert(1, '/home/p/polivajh/prosvet/public_html/venv/lib/python3.10/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'pro_core.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
